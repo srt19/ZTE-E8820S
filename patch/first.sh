422be9daf901b5aa0b3bc26d45d27e40df7511dd
srt19 <noobhek@gmail.com>
2025-07-13 01:26:52 +0700

    removed some files

--- patch/first.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mv patch/99-default-settings openwrt/package/base-files/files/etc/uci-defaults
-mv patch/mt7621_zte_e8820s.dts openwrt/target/linux/ramips/dts
-
-cat zte.txt >> openwrt/target/linux/ramips/image/mt7621.mk
